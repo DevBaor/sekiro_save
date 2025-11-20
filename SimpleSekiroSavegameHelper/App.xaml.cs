c379576e2a89fda63391c0d3726465544dedcac6
DevBaor <baotranduy666666@gmail.com>
2025-11-20 09:42:03 +0700

    Save game Sekiro

---
+++ SimpleSekiroSavegameHelper/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace SimpleSekiroSavegameHelper
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ SimpleSekiroSavegameHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("SimpleSekiroSavegameHelper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("uberhalit")]
+[assembly: AssemblyProduct("SimpleSekiroSavegameHelper")]
+[assembly: AssemblyCopyright("Copyright © uberhalit 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, 
+    ResourceDictionaryLocation.SourceAssembly 
+)]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
